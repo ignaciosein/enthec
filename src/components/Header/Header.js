3f1e1a8f5e6643fddcd71b7932161083e82089df
Ignacio <ignaciosein@gmail.com>
2021-09-05 12:00:33 +0200

    menuBurger funcional

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,44 @@
-import React,{useState, useEffect} from 'react'
-import "./Header.scss"
-import Logo from "../../img/logo.png"
-import MenuBurger from "../../img/menuburger.png"
+import React, { useState, useEffect } from "react";
+import "./Header.scss";
+import Logo from "../../img/logo.png";
+import MenuBurger from "../../img/menuburger.png";
 const Header = () => {
+  const [menu, setMenu] = useState(false);
 
-  const [menu, setMenu] = useState(false)
-
- 
- 
-/* const menuBurger = (estado) =>{
- 
-  if(estado){
-
- 
-    alert("se muestra menu")
-  }
-  else{
-
- alert("se oculta estado")
-
-  }
-
-} */
- 
- const paint=()=> `<div className="menu-list">
-  <ul>
-  <li>HOME</li>
-  <li>CONTACT</li>
-  <li>ABOUT</li>
-    <li>PRICING</li>
-   </ul>
-    </div>`
- 
-
-
-console.log(menu)
+  const paint = () => {
+    if (menu == true) {
+      return (
+        <div className="menu-list">
+          <ul>
+            <li>HOME</li>
+            <li>CONTACT</li>
+            <li>ABOUT</li>
+            <li>PRICING</li>
+          </ul>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="Header">
-  
       <div className="container-header">
         <div className="logo-header">
-        <h3>CODE-EXPRESS </h3> 
-          <img  src={Logo}></img>
+          <h3>CODE-EXPRESS </h3>
+          <img src={Logo}></img>
         </div>
         <div className="menu-burger">
-          <img onClick={ ()=> { setMenu(true)}} src={MenuBurger}></img>
+          <img
+            onClick={() => {
+              setMenu(!menu);
+            }}
+            src={MenuBurger}
+          ></img>
         </div>
       </div>
-     {menu==true? 
-     paint(): ""}
-
+      {paint()}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
